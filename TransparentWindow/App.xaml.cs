3d4a2b3e5613b602570fe1f59066f08c57f996cc
Deachearth <Liu-DaCheng@outlook.com>
2022-08-28 21:14:22 +0800

    Transparent Window

---
+++ TransparentWindow/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿global using System.Threading.Tasks;
+global using System.Windows;
+global using System.Windows.Shell;
+
+namespace TransparentWindow;
+
+public partial class App : Application
+{
+}
---
+++ TransparentWindow/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace TransparentWindow;
+
+public partial class MainWindow : Window
+{
+    public MainWindow ()
+    {
+        this.InitializeComponent();
+        this.Loaded += this.MainWindow_Loaded;
+    }
+
+    private async void MainWindow_Loaded ( object sender, RoutedEventArgs e )
+    {
+        this.Loaded -= this.MainWindow_Loaded;
+        await Task.Delay( 250 );
+
+        var winChrome = WindowChrome.GetWindowChrome( this );
+        winChrome.GlassFrameThickness = new( 0 );
+    }
+}
